429d929bbfbc86201100857927ce4b9e24923c27
zhaoxiaochun-art <337416641@qq.com>
2021-12-03 16:37:01 +0800

    update

--- PRT/MyLabels.h
+++ PRT/MyLabels.h
@@ -9,6 +9,7 @@ signals:
 private:
 	QLabel*lb;	
 	QMovie *movie;
+	int i = 0;
 	QString AppPath = qApp->applicationDirPath();
 public:
 	bool b_scale = false;
@@ -16,7 +17,6 @@ public:
 		: QLabel(parent)
 	{
 		lb = new QLabel(parent);
-		lb->setVisible(false);
 		pa = parent;
 		//movie = new QMovie("D:/5.gif");
 		movie = new QMovie(AppPath+"/ico/5.gif");
@@ -26,23 +26,44 @@ public:
 		lb->setAlignment(Qt::AlignCenter);
 		lb->setMovie(movie);
 		movie->start();
-		//lb->setScaledContents(true);
+		lb->setScaledContents(true);
 		lb->lower();
 	};
 	QWidget *pa;
 public slots:
 	void mousePressEvent(QMouseEvent*)
 	{
+		if (i==0)
+		{
+			i = 1;
+			lb->setVisible(false);
+		}
+		else if (i==1)
+		{
+			i = 2; 
+			lb->setFixedSize(60, 60);
+			lb->move((pa->width() - 50) / 2, 20);
+			lb->setPixmap(AppPath + "/ico/dr.ico");
+			lb->setVisible(true);
+		}
+		else
+		{
+			i = 0; 
+			lb->move(0, -200);
+			lb->setFixedSize(483, 366);
+			lb->setMovie(movie);
+			lb->setVisible(true);
+		}
 	};
 	void enterEvent(QEvent *e)
 	{
 		//this->setText(QString::fromLocal8Bit("»¶Ó­Ê¹ÓÃ"));
 
 
-		lb->setVisible(true);		
+		//lb->setVisible(true);		
 	}
 	void leaveEvent(QEvent *e)
 	{
-		lb->setVisible(false);
+		//lb->setVisible(false);
 	}
 };
