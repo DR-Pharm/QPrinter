9bee9281d94944c1d392ef6c6c54e482d29c8689
zhaoxiaochun-art <337416641@qq.com>
2021-12-10 11:51:30 +0800

    fusion

--- PRT/main.cpp
+++ PRT/main.cpp
@@ -2,6 +2,7 @@
 #include <QtWidgets/QApplication>
 #include "WindowOut.h"
 
+#include <QStyleFactory>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -9,6 +10,7 @@ int main(int argc, char *argv[])
 	WindowOut* levelOut = new WindowOut();
 	levelOut->setWindowCount(0);//动画窗弹出数，防止覆盖
 
+	QApplication::setStyle(QStyleFactory::create("Fusion"));//界面风格
 	if (!mem.create(1))//创建共享内存mem，如果该共享内存已存在，则弹出提示对话框，并退出
 	{
 		levelOut->getString(QString::fromLocal8Bit("系统已运行一实例，请勿重复开启！"), 2000);
